02bcb2086e18489a11da718231461956ce0a0807
sancam2007-lang <sancam2007@gmail.com>
2026-08-27 22:36:07 -0700

    Delete src directory

--- src/components/paper/Stamp.tsx
+++
@@ -1,8 +0,0 @@
-export function Stamp({ text, sub }: { text: string; sub?: string }) {
-  return (
-    <div className="stamp stamp--thud" role="img" aria-label={`Stamped: ${text}`}>
-      {text}
-      {sub ? <small>{sub}</small> : null}
-    </div>
-  );
-}
